5e3cbe512bca78fbf65412072fb1a14e1fbf64e7
bobbysherwin <bobsher1515@hotmail.com>
2018-01-29 07:15:41 -0800

    user input messages

--- hw3.py
+++ hw3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-'''
+"""
 Ask the user to enter a starting and ending number
 The user must not enter a starting number less than 1
 The user must enter an ending number at least 5 times greater than the starting number
@@ -10,12 +10,22 @@ Print out the number and index of each item in the list that is even
 Sum all the odd numbers in the list using a for loop ( hint: append odd numbers to a list and then sum() that list )
 Print out the cumulative sum calculated above
  Upload your python file to Canvas.
-'''
+"""
 
-start_num = input("Enter a number to start with: ")
-start_num = int(start_num)
 
-if start_num <= 1:
-    print("Sorry.  The number must be more than 1.  Try again.")
+def user_numbers():
+    start_num = input("Enter a number to start with: ")
+    start_num = int(start_num)
 
-end_num = input("Enter a number to end with: ")
+    if start_num <= 1:
+        print("The number must be more than 1.  Try again.")
+
+    end_num = input("Enter a number to end with: ")
+    end_num = int(end_num)
+
+    if end_num < start_num * 5:
+        print("The ending number must be at least five times greater than the starting number.  Try again.")
+
+
+user_numbers()
+
