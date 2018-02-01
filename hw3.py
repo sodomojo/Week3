cd0ce557560aae70803313b4abc0c05d909052fe
bobbysherwin <bobsher1515@hotmail.com>
2018-01-31 18:48:49 -0800

    - added comments and docstrings - fixed sum()

--- hw3.py
+++ hw3.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import math
+
 
 """
 Ask the user to enter a starting and ending number
@@ -13,27 +15,40 @@ Print out the cumulative sum calculated above
 
 """
 
+# initialize the start and end of the list range
 start_num = 0
 end_num = 0
 
 start_num = int(input("Please enter a starting number: "))
+# if the user enters a number less than zero throw an error and retry.
 while int(start_num) < 0:
     print("The number must be more than 1.  Try again.")
 
-
 end_num = int(input("Please enter an ending number: "))
+# if the user enters a number less than 5x the range at the start of the list throw an error and retry.
 while int(end_num) < (start_num * 5):
     print("The ending number must be at least five times greater than the starting number.  Try again.")
 
-
+#compute the full range based on the start and ended input values
 full_range = list(range(start_num, end_num))
 
-print("Even numbers in your list:")
+#loop through full range and pull out all even numbers and their index.  Adding +1 so that the index starts at 1, not zero
+print("Even numbers in your list: \n")
 for even_index, i in enumerate(full_range):
     if i % 2 == 0:
-        print("{} is at the {} index".format(i, str(even_index)))
-        break
-for odd_index, i in enumerate(full_range):
-    if i % 2 == 1:
+        print("{} is at the {} index\n".format(i, str(even_index+1)))
+
+#create empty list that will eventually be all odd numbers
+odd_sum = []
+#find all odd numbers in range by finding numbers with a remainder of 1.  Add to odd num list
+for j in full_range:
+    if j % 2 == 1:
+        odd_sum.append(j)
+
+#print the sum of all the odd numbers
+print("The sum of your number list is: {}".format(sum(odd_sum)))
+
+input("Press any key to quit")
+
 
 
