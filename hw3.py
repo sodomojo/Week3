fe96b872fa4bc6142b647b41dc735972dacd832a
bobbysherwin <bobsher1515@hotmail.com>
2018-01-31 22:37:39 -0800

    Finally fixed the last number in the range bug.  Done!

--- hw3.py
+++ hw3.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python
 
 """
-Ask the user to enter a starting and ending number
-The user must not enter a starting number less than 1
-The user must enter an ending number at least 5 times greater than the starting number
-Create checks and error messages for the above
-Create a list of integers in the range of the user's starting and ending numbers
-Print out the number and index of each item in the list that is even
-Sum all the odd numbers in the list using a for loop ( hint: append odd numbers to a list and then sum() that list )
-Print out the cumulative sum calculated above
- Upload your python file to Canvas.
+This script will do the following
+
+- Ask the user to enter a starting and ending number
+- Check that the starting number is not less than 1
+- Check that the ending number is at least 5 times greater than the starting number
+- Create a list of integers in the range of the user's starting and ending numbers
+- Print out the number and index of each item in the list that is even
+- Sum and print all the odd numbers in the list
 
 """
 # function to obtain the range start
@@ -47,7 +46,8 @@ first_number()
 end_number()
 
 # compute the full range based on the start and ended input values
-full_range = list(range(start_num, end_num))
+# add +1 to end range to make sure last number is counted
+full_range = list(range(start_num, end_num+1, 1))
 
 # loop through full range and pull out all even numbers and their index.
 # Adding +1 so that the index starts at 1, not zero
