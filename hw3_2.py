2b01cbf9ce149ece9eaa3318d287230138209ac7
bobbysherwin <bobsher1515@hotmail.com>
2018-01-31 21:40:01 -0800

    ok, redid everything...! - moved the start and end ranges into their own functions - removed the - added additional checks to verify that the user enters a number greater than 1 and 5 times the start input

--- hw3_2.py
+++ hw3_2.py
@@ -1,20 +1,6 @@
-#!/usr/bin/env python
-
-"""
-Ask the user to enter a starting and ending number
-The user must not enter a starting number less than 1
-The user must enter an ending number at least 5 times greater than the starting number
-Create checks and error messages for the above
-Create a list of integers in the range of the user's starting and ending numbers
-Print out the number and index of each item in the list that is even
-Sum all the odd numbers in the list using a for loop ( hint: append odd numbers to a list and then sum() that list )
-Print out the cumulative sum calculated above
- Upload your python file to Canvas.
-
-"""
-
 # initialize the start and end of the list range
-
+start_num = 0
+end_num = 0
 
 def first_number():
     # if the user enters a number less than zero throw an error and retry.
@@ -59,3 +45,6 @@ for j in full_range:
 print("The sum of your number list is: {} \n".format(sum(odd_sum)))
 
 input("Press any key to quit")
+
+
+
